5d7e4b99ef7b2990eb79983d7e0b50126c0c0777
atakannguvenn <atakanhero@hotmail.com>
2019-05-12 07:10:08 -0700

    name change?

--- App.js
+++ App.js
@@ -22,7 +22,7 @@ export default class App extends Component<Props> {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to Atakan's App</Text>
-        <Text style={styles.instructions}>but he has no idea what he is doing...</Text>
+        <Text style={styles.instructions}>but he is kinda lost...</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
     );
