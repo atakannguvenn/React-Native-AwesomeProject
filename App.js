07ad52e4012486310affe151d3eff1ce0b7729f3
atakannguvenn <atakanhero@hotmail.com>
2019-05-12 23:57:57 +0800

    Android Build - Windows Update

--- App.js
+++ App.js
@@ -12,8 +12,8 @@ import {Platform, StyleSheet, Text, View} from 'react-native';
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+    'Double R on your keyboard to reload,\n' +
+    'CTRL + M for dev menu',
 });
 
 type Props = {};
