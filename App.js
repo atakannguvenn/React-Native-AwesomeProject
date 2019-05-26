b33b6906c5e50c3ffbe4cc1bc409252b67a3dee0
atakannguvenn <atakanhero@hotmail.com>
2019-05-26 12:00:14 +0800

    tutorials_10_2

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, TextInput, View, Image, TouchableOpacity, StatusBar} from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -16,14 +16,29 @@ const instructions = Platform.select({
     'CTRL + M for dev menu',
 });
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
+
+
   render() {
     return (
-      <View style={styles.container}>
+      <View behavior = "padding" style={styles.container}>
+        <StatusBar
+          barStyle = "default"
+          >
+          </StatusBar>
         <Text style={styles.welcome}>Welcome to Atakan's App</Text>
-        <Text style={styles.instructions}>and he is learning..</Text>
-        <Text style={styles.instructions}>but now he needs sleep...</Text>
+        <Text style={styles.instructions}>But now he needs sleep...</Text>
+        <Text style={styles.instructions}>How many hour of sleep do u think he needs?</Text>
+        <TextInput 
+          style = {styles.input}
+          placeholder = "Enter here"
+          placeholderTextColor = "#5d7ba0"
+          > 
+        </TextInput>
+        <TouchableOpacity style = {styles.button}>
+          <Text style = {styles.buttontext}>ENTER</Text>
+        </TouchableOpacity>
+        <Image source = {{uri: 'https://bit.ly/30My8E3'}} style={{width: 380, height: 200}}></Image>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
     );
@@ -35,16 +50,35 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#1eadff',
   },
   welcome: {
-    fontSize: 20,
+    fontSize: 25,
     textAlign: 'center',
     margin: 10,
+    color: "#0068a3"
   },
   instructions: {
     textAlign: 'center',
-    color: '#333333',
+    color: '#FFF',
     marginBottom: 5,
   },
+  input: {
+    color: '#0068a3',
+    backgroundColor: '#cce9ff',
+    width: 200,
+    height: 40,
+    marginBottom: 10,
+  },
+  button: {
+    backgroundColor: '#0068a3',
+    paddingVertical: 10,
+    marginBottom: 10,
+    width: 75,
+  },
+  buttontext: {
+    textAlign: 'center',
+    color: '#FFF',
+    fontWeight: '700',
+  }
 });
